1f88dc50e8ee7a1fb909e321f81c04e63f5511fe
Sebastian Prestele <sebastian.prestele@gmail.com>
2022-03-09 15:43:30 +0100

    Added all CRUD methods to products and users

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -4,6 +4,8 @@ import {
   createUser,
   findUsers,
   findUserById,
+  updateUser,
+  deleteUser,
 } from '../controllers/userController'
 
 const router = Router()
@@ -11,5 +13,7 @@ const router = Router()
 router.get('/', findUsers)
 router.post('/', createUser)
 router.get('/:userId', findUserById)
+router.put('/:userId', updateUser)
+router.delete('/:userId', deleteUser)
 
 export default router
