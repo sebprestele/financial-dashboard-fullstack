36df784b721484f49de7e1bcfa6dd5886a63dfbb
Sebastian Prestele <sebastian.prestele@gmail.com>
2022-03-10 23:08:51 +0100

    Setup basic frontend and connected to backend

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./components/Home/Home";
+import User from "./components/User/User";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/user" element={<User />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
