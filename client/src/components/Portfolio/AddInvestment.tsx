06ae0c1ba34d8195d97a8982ce6116194a49d541
Sebastian Prestele <Sebastian Prestele>
2022-03-28 23:01:39 +0200

    Add Investment, Edit Investment and Table done

--- client/src/components/Portfolio/AddInvestment.tsx
+++ client/src/components/Portfolio/AddInvestment.tsx
@@ -10,7 +10,6 @@ import {
 import { useForm, formList } from "@mantine/form";
 import { DatePicker } from "@mantine/dates";
 import axios from "axios";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 import { setSingleUser } from "../../Redux/userSlice";
@@ -18,35 +17,30 @@ import { setAltModalState } from "../../Redux/helperSlice";
 import {
   category,
   currency,
-  cryptoCurrency,
+  transactionType,
 } from "../../data/TransactionsData";
 
 function AddInvestment() {
-  //States for the conditional form field logic
-  const [categoryActive, setCategoryActive] = useState("Crypto");
-  const [transactionType, setTransactionType] = useState("Buy");
   const userId = localStorage.getItem("userId");
   const token = localStorage.getItem("currentToken");
   const dispatch = useDispatch();
 
   const form = useForm({
     initialValues: {
-      name: "",
-      category: "Crypto",
+      name: "Random Investment",
+      category: "Uncategorized",
       transactionType: "Buy",
       amount: 0,
       quantity: 0,
       date: formList([{ dateBought: new Date(), dateSold: new Date() }]),
       price: formList([{ priceBought: 0, priceSold: 0 }]),
       currency: "EUR",
-      cryptoCurrency: "Bitcoin",
       fee: 0,
       comments: "",
     },
 
     validate: (values) => ({
       name: values.name === undefined ? "Name is required" : null,
-      category: values.amount === undefined ? "Category is required" : null,
     }),
   });
 
@@ -59,7 +53,6 @@ function AddInvestment() {
             await axios
               .post(`http://localhost:5000/api/v1/investment/${userId}`, values)
               .then((res) => console.log(res));
-            form.reset();
             await fetch(`http://localhost:5000/api/v1/users/${userId}`, {
               method: "GET",
               headers: { Authorization: `Bearer ${token}` },
@@ -71,7 +64,7 @@ function AddInvestment() {
           } catch (error) {
             console.log(error);
           }
-          setCategoryActive("Crypto");
+          form.reset();
           dispatch(setAltModalState());
         })}
       >
@@ -83,97 +76,49 @@ function AddInvestment() {
         />
         <Select
           style={{ marginTop: 10, zIndex: 2 }}
-          data={["Buy", "Sell"]}
+          data={transactionType}
           placeholder="Type of transaction"
           label="Type of transaction"
           {...form.getInputProps("transactionType")}
-          onChange={(value) => {
-            value !== null && setTransactionType(value);
-          }}
-          value={transactionType}
         />
         <Select
-          required
           style={{ marginTop: 10, zIndex: 2 }}
           data={category}
           placeholder="Pick a category"
           label="Category of your investment"
           {...form.getInputProps("category")}
-          onChange={(value) => {
-            value !== null && setCategoryActive(value);
-          }}
-          value={categoryActive}
         />
-        {transactionType === "Buy" ? (
-          <DatePicker
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Date of transaction"
-            label="Date Bought"
-            {...form.getListInputProps("date", 0, "dateBought")}
-          />
-        ) : (
-          <DatePicker
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Date of transaction"
-            label="Date Sold"
-            {...form.getListInputProps("date", 0, "dateSold")}
-          />
-        )}
-
-        {categoryActive === "Crypto" ? (
-          <Select
-            style={{ marginTop: 10, zIndex: 2 }}
-            data={cryptoCurrency}
-            placeholder="Pick a coin"
-            label="Cryptocurrency"
-            {...form.getInputProps("cryptoCurrency")}
-          />
-        ) : (
-          <Select
-            style={{ marginTop: 10, zIndex: 2 }}
-            data={currency}
-            placeholder="Pick a currency"
-            label="Currency"
-            {...form.getInputProps("currency")}
-          />
-        )}
-        {categoryActive !== "Stocks" &&
-        categoryActive !== "Crypto" &&
-        categoryActive !== "ETF" &&
-        categoryActive !== "Bonds" ? (
-          <NumberInput
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Amount"
-            label={
-              categoryActive === "Real Estate"
-                ? "Property Value"
-                : "Total Amount"
-            }
-            {...form.getInputProps("amount")}
-          />
-        ) : (
-          <NumberInput
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Quantity"
-            label="Quantity"
-            {...form.getInputProps("quantity")}
-          />
-        )}
-        {transactionType === "Buy" ? (
-          <NumberInput
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Price"
-            label="Price Bought"
-            {...form.getListInputProps("price", 0, "priceBought")}
-          />
-        ) : (
-          <NumberInput
-            style={{ marginTop: 10, zIndex: 2 }}
-            placeholder="Price"
-            label="Price Sold"
-            {...form.getListInputProps("price", 0, "priceSold")}
-          />
-        )}
+        <DatePicker
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Date of transaction"
+          label="Date of transaction"
+          {...form.getListInputProps("date", 0, "dateBought")}
+        />
+        <Select
+          style={{ marginTop: 10, zIndex: 2 }}
+          data={currency}
+          placeholder="Pick a currency"
+          label="Currency"
+          {...form.getInputProps("currency")}
+        />
+        <NumberInput
+          decimalSeparator="."
+          step={0.01}
+          precision={5}
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Quantity"
+          label="Quantity"
+          {...form.getInputProps("quantity")}
+        />
+        <NumberInput
+          decimalSeparator="."
+          precision={2}
+          step={0.01}
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Price"
+          label="Price Bought / Sold"
+          {...form.getListInputProps("price", 0, "priceBought")}
+        />
         <NumberInput
           style={{ marginTop: 10, zIndex: 2 }}
           placeholder="Fee"
@@ -186,7 +131,6 @@ function AddInvestment() {
           placeholder="Add additional comments"
           {...form.getInputProps("comments")}
         />
-
         <Group position="right" mt="md">
           <Button type="submit">Submit</Button>
         </Group>
--- client/src/components/Portfolio/EditInvestment.tsx
+++ client/src/components/Portfolio/EditInvestment.tsx
@@ -1,12 +1,17 @@
-import { Button, Group, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { Button, Group, NumberInput, Select, TextInput } from "@mantine/core";
+import { formList, useForm } from "@mantine/form";
 import { DatePicker } from "@mantine/dates";
-import { CurrencyEuro } from "tabler-icons-react";
-import { RowData } from "./PortfolioOverviewTable";
-import axios from "axios";
 import { useDispatch } from "react-redux";
+import axios from "axios";
+
+import { RowData } from "./PortfolioOverviewTable";
 import { setSingleUser } from "../../Redux/userSlice";
 import { setModalState } from "../../Redux/helperSlice";
+import {
+  category,
+  currency,
+  transactionType,
+} from "../../data/TransactionsData";
 
 const EditInvestment = (rowDetails: RowData) => {
   const userId = localStorage.getItem("userId");
@@ -22,10 +27,14 @@ const EditInvestment = (rowDetails: RowData) => {
       category: rowDetails.category,
       transactionType: rowDetails.transactionType,
       quantity: rowDetails.quantity,
-      /*  date: formList([{ dateBought: rowDetails.dateBought, dateSold: rowDetails.dateSold }]),
+      date: formList([
+        {
+          dateBought: rowDetails.date[0].dateBought,
+          dateSold: rowDetails.date[0].dateSold,
+        },
+      ]),
       price: formList([{ priceBought: rowDetails.price, priceSold: 0 }]),
-      currency: rowDetails.currency, */
-      cryptoCurrency: rowDetails.cryptoCurrency,
+      currency: rowDetails.currency,
       fee: rowDetails.fee,
       comments: rowDetails.comments,
     },
@@ -33,10 +42,12 @@ const EditInvestment = (rowDetails: RowData) => {
     validate: (values) => ({
       name: values.name === undefined ? "Name is required" : null,
       amount: values.amount === undefined ? "Amount is required" : null,
-      //date: values.date === undefined ? "Date is required" : null,
+      date: values.date === undefined ? "Date is required" : null,
       category: values.category === undefined ? "Category is required" : null,
     }),
   });
+
+  console.log(rowDetails.date[0].dateBought);
 
   return (
     <>
@@ -56,6 +67,7 @@ const EditInvestment = (rowDetails: RowData) => {
               .then((res) => res.json())
               .then((data) => {
                 dispatch(setSingleUser(data));
+                console.log(data);
               });
           } catch (error) {
             console.log(error);
@@ -69,30 +81,60 @@ const EditInvestment = (rowDetails: RowData) => {
           placeholder={rowDetails.name}
           {...form.getInputProps("name")}
         />
-        <TextInput
-          required
-          icon={<CurrencyEuro size={16} />}
-          label="Amount"
-          // placeholder={rowDetails.amount}
-          {...form.getInputProps("amount")}
+        <Select
+          style={{ marginTop: 10, zIndex: 2 }}
+          data={transactionType}
+          placeholder="Type of transaction"
+          label="Type of transaction"
+          {...form.getInputProps("transactionType")}
         />
         <DatePicker
-          required
-          label="Date"
-          //   {...form.getInputProps("date")}
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder={rowDetails.date[0].dateBought.slice(0, 10)}
+          label="Date of transaction"
+          {...form.getListInputProps("date", 0, "dateBought")}
         />
-        <TextInput
-          required
-          label="Category"
-          //  placeholder={rowDetails.category}
+        <Select
+          style={{ marginTop: 10, zIndex: 2 }}
+          data={category}
+          placeholder="Pick a category"
+          label="Category of your investment"
           {...form.getInputProps("category")}
+        />
+        <Select
+          style={{ marginTop: 10, zIndex: 2 }}
+          data={currency}
+          placeholder="Pick a currency"
+          label="Currency"
+          {...form.getInputProps("currency")}
+        />
+        <NumberInput
+          decimalSeparator="."
+          step={0.01}
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Quantity"
+          label="Quantity"
+          {...form.getInputProps("quantity")}
+        />
+        <NumberInput
+          decimalSeparator="."
+          step={0.01}
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Price"
+          label="Price Bought / Sold"
+          {...form.getListInputProps("price", 0, "priceBought")}
+        />
+        <NumberInput
+          style={{ marginTop: 10, zIndex: 2 }}
+          placeholder="Fee"
+          label="Add any fees"
+          {...form.getInputProps("fee")}
         />
         <TextInput
           label="Comments"
           placeholder={rowDetails.comments}
           {...form.getInputProps("comments")}
         />
-
         <Group mt={10}>
           <Button type="submit" name="save">
             Save Details
@@ -122,7 +164,7 @@ const EditInvestment = (rowDetails: RowData) => {
               dispatch(setModalState());
             }}
           >
-            Delete Expense
+            Delete Transaction
           </Button>
         </Group>
       </form>
