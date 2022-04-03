ed218eec780aa125a86871a4692cdc2f784ed426
Sebastian Prestele <Sebastian Prestele>
2022-04-04 01:34:35 +0200

    Added Procfile again

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import app from './src/app'
+import app from './app'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const mongoUri = process.env.MONGODB_URI!
