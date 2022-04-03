a5cf17025af1c7a1cf793b5882bea079589ed959
Sebastian Prestele <Sebastian Prestele>
2022-04-04 00:45:50 +0200

    Changed app.listen Port address

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
-import app from './app'
+import mongoose from "mongoose";
+import app from "./server/src/app";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const mongoUri = process.env.MONGODB_URI!
+const mongoUri = process.env.MONGODB_URI!;
 
 mongoose
   .connect(mongoUri, {
@@ -12,12 +12,12 @@ mongoose
     autoIndex: true,
   })
   .then(() => {
-    app.listen(process.env.PORT, () =>
-      console.log(`App running on port ${process.env.PORT || 5000}`)
-    )
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`App running on port ${process.env.PORT}`)
+    );
   })
   .catch((error) => {
     console.log(
-      'Mongodb connection error. Plz make sure your mongodb is running.' + error
-    )
-  })
+      "Mongodb connection error. Plz make sure your mongodb is running." + error
+    );
+  });
